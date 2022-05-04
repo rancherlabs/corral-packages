ab0ffef891bb1e89cdf523a8f8f62789a8f36cbe
Jake Hyde <jakefhyde@gmail.com>
2022-05-04 19:52:56 -0400

    Updated build script

---
+++ go.mod
@@ -0,0 +1,80 @@
+module github.com/rancher/corral-packages/scripts
+
+go 1.17
+
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/containerd/containerd v1.6.4 // indirect
+	github.com/docker/cli v20.10.14+incompatible // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.14+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.6.4 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-metrics v0.0.1 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-version v1.4.0 // indirect
+	github.com/hashicorp/hc-install v0.3.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/terraform-exec v0.16.1 // indirect
+	github.com/hashicorp/terraform-json v0.13.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.3.1 // indirect
+	github.com/klauspost/compress v1.15.2 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/locker v1.0.1 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/sftp v1.13.4 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.34.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rancherlabs/corral v1.0.0-rc5 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.0.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.11.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/zclconf/go-cty v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.24.0 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	oras.land/oras-go v1.1.1 // indirect
+)
--- scripts/build.go
+++ scripts/build.go
@@ -1,22 +1,275 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	corralpkg "github.com/rancherlabs/corral/pkg/package"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
 
+type Manifest struct {
+	Name        string
+	Description string
+}
+
 type Package struct {
-	Manifest  interface{}
+	Manifest  Manifest
 	Templates []string
 	Variables map[string][]string
 }
 
 func main() {
+	log.Info("Removing existing packages")
+	err := os.RemoveAll("dist")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("Creating dist folder")
+	err = os.MkdirAll("dist", 0775)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 	filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
 
-		f, err := os.Open(path)
+		log.Infof("Processing %s", path)
+
+		body, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var p Package
+		if err = yaml.Unmarshal(body, &p); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Info("Loading templates")
+
+		packages := make([]corralpkg.Package, len(p.Templates))
+		for i, t := range p.Templates {
+			log.Debugf("Loading template for %s", t)
+			merge, err := corralpkg.LoadPackage(filepath.Join("templates", t))
+			if err != nil {
+				log.Fatal(err)
+			}
+			packages[i] = merge
+		}
+
+		// For nested packages, just use top level name
+		for i, t := range p.Templates {
+			idx := strings.IndexRune(t, '/')
+			if idx != -1 {
+				p.Templates[i] = t[0:idx]
+			}
+		}
+
+		tmplName := strings.Join(p.Templates, "-")
+
+		log.Infof("Generating staging template %s", tmplName)
+
+		tmpl, err := corralpkg.MergePackages(filepath.Join("staging", tmplName), packages)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// todo this logic is duplicated by corral/cmd/package.Template, fix
+		for i := range packages {
+			if i > 0 {
+				tmpl.Description += "\n"
+			}
+
+			if packages[i].Description != "" {
+				tmpl.Description += packages[i].Description
+			}
+		}
+		tmpl.Name = filepath.Base(tmplName)
+
+		buf, _ := yaml.Marshal(tmpl)
+
+		err = os.WriteFile(filepath.Join("staging", tmplName, "manifest.yaml"), buf, 0664)
+		if err != nil {
+			log.Fatal("failed to write manifest: ", err)
+		}
+
+		log.Info("Generating variable manifests")
+
+		err = os.MkdirAll("variables", 0775)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tmplPkg, err := corralpkg.LoadPackage(filepath.Join("staging", tmplName))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		varPkgs := map[string][]corralpkg.Package{}
+
+		for varName, vars := range p.Variables {
+			log.Infof("Generating variable manifests for %s", varName)
+			err = os.MkdirAll(filepath.Join("variables", varName), 0775)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			type VariableManifest struct {
+				RootPath    string
+				Name        string
+				Description string
+				Variables   map[string]interface{}
+			}
+
+			for _, v := range vars {
+				// since the name will be part of the image name, ensure it is compatible
+				r := regexp.MustCompile("[^A-Za-z-_0-9.]")
+				name := r.ReplaceAllString(v, "-")
+
+				log.Debugf("Generating variable manifests for %s to pin at %s", varName, v)
+				varManifest := VariableManifest{
+					RootPath: filepath.Join("variables", varName, name),
+					Name:     name,
+					Description: fmt.Sprintf("%s is pinned to %s.", varName, v),
+					Variables: map[string]interface{}{
+						varName: struct {
+							ReadOnly bool `yaml:"readOnly"`
+							Default  interface{}
+						}{
+							ReadOnly: true,
+							Default:  v,
+						},
+					},
+				}
+				buf, err = yaml.Marshal(varManifest)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				varRoot := filepath.Join("variables", varName, name)
+
+				err = os.MkdirAll(varRoot, 0775)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// todo this logic is duplicated by corral/cmd/package.Template, fix
+				err = os.WriteFile(filepath.Join(varRoot, "manifest.yaml"), buf, 0664)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				varPkg, err := corralpkg.LoadPackage(varRoot)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if _, ok := varPkgs[varName]; !ok {
+					varPkgs[varName] = []corralpkg.Package{}
+				}
+				varPkgs[varName] = append(varPkgs[varName], varPkg)
+			}
+		}
+
+		// This order isn't strictly perfect according to the yaml definition due to map iteration order, so this is
+		// currently a best effort approximation assuming the variables should be ordered alphabetically.
+		varMatrix := make([][]corralpkg.Package, len(varPkgs))
+		keys := make([]string, len(varPkgs))
+		{
+			i := 0
+			for k := range varPkgs {
+				keys[i] = k
+				i++
+			}
+		}
+		sort.Strings(keys)
+		for i, v := range keys {
+			varMatrix[i] = varPkgs[v]
+		}
+
+		product := cartesianProductN[corralpkg.Package](varMatrix)
+		for _, vars := range product {
+			names := []string{tmplName}
+			pkgs := []corralpkg.Package{tmplPkg}
+			for _, v := range vars {
+				names = append(names, v.Name)
+				pkgs = append(pkgs, v)
+			}
+
+			name := strings.Join(names, "-")
+
+			tmpl, err = corralpkg.MergePackages(filepath.Join("dist", name), append([]corralpkg.Package{tmplPkg}, vars...))
+			if err != nil {
+				log.Fatal(err)
+			}
+			tmpl.Description = tmplPkg.Description
+
+			// todo this logic is duplicated by corral/cmd/package.Template, fix
+			for i := range vars {
+				if len(tmpl.Description) > 0 {
+					tmpl.Description += "\n\n"
+				}
+
+				tmpl.Description += vars[i].Description
+			}
+			tmpl.Name = name
+
+			buf, err = yaml.Marshal(tmpl)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = os.WriteFile(filepath.Join("dist", name, "manifest.yaml"), buf, 0664)
+			if err != nil {
+				logrus.Fatal("failed to write manifest: ", err)
+			}
+		}
+
+		return nil
 	})
+
+	err = os.RemoveAll("staging")
+	if err != nil {
+		log.Warnf("Could not remove staging directory")
+	}
+
+	err = os.RemoveAll("variables")
+	if err != nil {
+		log.Warnf("Could not remove variables directory")
+	}
+}
+
+// create a n-ary cartesian product where the dimension is equal to len(t)
+func cartesianProductN[T any](t [][]T) [][]T {
+	if len(t) == 0 {
+		return [][]T{}
+	}
+	return cartesianProductNHelper[T]([]T{}, t)
+}
+
+func cartesianProductNHelper[T any](t1 []T, t [][]T) [][]T {
+	ret := [][]T{}
+	if len(t) == 1 {
+		for _, v := range t[0] {
+			ret = append(ret, append(t1, v))
+		}
+		return ret
+	}
+	for _, v := range t[0] {
+		tmp := cartesianProductNHelper(append(t1, v), t[1:])
+		ret = append(ret, tmp...)
+	}
+
+	return ret
 }
