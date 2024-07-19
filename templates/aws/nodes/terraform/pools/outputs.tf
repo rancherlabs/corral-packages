560f375a1cd7a7cd4c49f01749ea3ecdc5721a38
Michael Spencer <MSpencer87@users.noreply.github.com>
2024-07-19 12:30:57 -0700

    Added rancher-proxy package, proxy changes for custom clusters and rke2 packages

--- templates/aws/nodes/terraform/pools/outputs.tf
+++ templates/aws/nodes/terraform/pools/outputs.tf
@@ -6,9 +6,10 @@ output "corral_node_pools" {
   value = {
     node = [for instance in aws_instance.node : {
       name = instance.tags.Name // unique name of node
-      user = "root" // ssh username
-      address = var.airgap_setup ? instance.private_ip : instance.public_ip // address of ssh host
-      bastion_address = var.airgap_setup ? var.bastion_ip : ""
+      user = var.proxy_setup ? "root" : var.aws_ssh_user // ssh username
+      address = var.airgap_setup || var.proxy_setup ? instance.private_ip : instance.public_ip // address of ssh host
+      bastion_address = var.airgap_setup || var.proxy_setup ? var.registry_ip : ""
+      bastion_internal_address = var.airgap_setup || var.proxy_setup ? var.registry_private_ip : ""
     }]
   }
 }
