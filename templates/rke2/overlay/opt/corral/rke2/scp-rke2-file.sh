3f07f223244f12e6c956fc47c223bd9baa4c0862
Josh Diamond <joshdiamondthedev@gmail.com>
2024-12-20 14:26:34 -0700

    support proxy with corral-packages

--- templates/rke2/overlay/opt/corral/rke2/scp-rke2-file.sh
+++ templates/rke2/overlay/opt/corral/rke2/scp-rke2-file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ "$CORRAL_airgap_setup" = true ]; then
+if [ "$CORRAL_airgap_setup" = true ] || [ "$CORRAL_proxy_setup" = true ]; then
     scp -r -o StrictHostKeyChecking=no root@"${CORRAL_registry_private_ip}":/root/rke2-artifacts /root/rke2-artifacts
 fi
